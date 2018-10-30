3efd3e7c6196268c12630a719d50e0180ba42163
xxxzzz <aaa@bbb.ccc>
2018-10-30 18:39:04 +0800

    normalizer fix; share between critics ( multiprocess ) aka converted to torch.Parameter; need to retest!

--- utils/normalizer.py
+++ utils/normalizer.py
@@ -7,9 +7,9 @@ Based on code from Marcin Andrychowicz
   >> encouraged to check that nice project
 """
 import numpy as np
+import torch
 
-
-class Normalizer(object):
+class Normalizer(torch.nn.Module):
     def __init__(
             self,
             size,
@@ -18,14 +18,17 @@ class Normalizer(object):
             mean=0,
             std=1,
     ):
+        super().__init__()
         self.size = size
         self.eps = eps
         self.default_clip_range = default_clip_range
-        self.sum = np.zeros(self.size, np.float32)
-        self.sumsq = np.zeros(self.size, np.float32)
-        self.count = np.ones(1, np.float32)
-        self.mean = mean + np.zeros(self.size, np.float32)
-        self.std = std * np.ones(self.size, np.float32)
+
+        self.sum = torch.nn.Parameter(torch.zeros(self.size))
+        self.sumsq = torch.nn.Parameter(torch.zeros(self.size))
+        self.count = torch.nn.Parameter(torch.ones(1))
+        self.mean = torch.nn.Parameter(mean + torch.zeros(self.size))
+        self.std = torch.nn.Parameter(std * torch.ones(self.size))
+
         self.synchronized = True
 
     def update(self, v):
@@ -33,41 +36,35 @@ class Normalizer(object):
             v = np.expand_dims(v, 0)
         assert v.ndim == 2
         assert v.shape[1] == self.size
-        self.sum += v.sum(axis=0)
-        self.sumsq += (np.square(v)).sum(axis=0)
-        self.count[0] += v.shape[0]
+        self.sum.data = self.sum.data + torch.tensor(v.sum(axis=0))
+        self.sumsq.data = self.sumsq.data + torch.tensor(np.square(v).sum(axis=0))
+        self.count[0] = self.count[0] + v.shape[0]
         self.synchronized = False
 
     def normalize(self, v, clip_range=None):
         if not self.synchronized:
-            self.synchronize()
+            self._synchronize()
         if clip_range is None:
             clip_range = self.default_clip_range
-        mean, std = self.mean, self.std
+
+        # convert back to numpy ( torch is just for sharing data between workers )
+        std = self.std.detach().numpy()
+        mean = self.mean.detach().numpy()
+
         if v.ndim == 2:
             mean = mean.reshape(1, -1)
             std = std.reshape(1, -1)
         return np.clip((v - mean) / std, -clip_range, clip_range)
 
-    def denormalize(self, v):
-        if not self.synchronized:
-            self.synchronize()
-        mean, std = self.mean, self.std
-        if v.ndim == 2:
-            mean = mean.reshape(1, -1)
-            std = std.reshape(1, -1)
-        return mean + v * std
-
-    def synchronize(self):
-        self.mean[...] = self.sum / self.count[0]
-        self.std[...] = np.sqrt(
+    def _synchronize(self):
+        self.mean.data = self.sum.detach() / self.count[0]
+        self.std.data = torch.sqrt(
             np.maximum(
                 np.square(self.eps),
-                self.sumsq / self.count[0] - np.square(self.mean)
+                self.sumsq.detach() / self.count.detach()[0] - (self.mean.detach() ** 2)
             )
         )
         self.synchronized = True
-
 
 class IdentityNormalizer(object):
     def __init__(self, *args, **kwargs):
@@ -78,51 +75,3 @@ class IdentityNormalizer(object):
 
     def normalize(self, v, clip_range=None):
         return v
-
-    def denormalize(self, v):
-        return v
-
-
-class FixedNormalizer(object):
-    def __init__(
-            self,
-            size,
-            default_clip_range=np.inf,
-            mean=0,
-            std=1,
-            eps=1e-8,
-    ):
-        assert std > 0
-        std = std + eps
-        self.size = size
-        self.default_clip_range = default_clip_range
-        self.mean = mean + np.zeros(self.size, np.float32)
-        self.std = std + np.zeros(self.size, np.float32)
-        self.eps = eps
-
-    def set_mean(self, mean):
-        self.mean = mean + np.zeros(self.size, np.float32)
-
-    def set_std(self, std):
-        std = std + self.eps
-        self.std = std + np.zeros(self.size, np.float32)
-
-    def normalize(self, v, clip_range=None):
-        if clip_range is None:
-            clip_range = self.default_clip_range
-        mean, std = self.mean, self.std
-        if v.ndim == 2:
-            mean = mean.reshape(1, -1)
-            std = std.reshape(1, -1)
-        return np.clip((v - mean) / std, -clip_range, clip_range)
-
-    def denormalize(self, v):
-        mean, std = self.mean, self.std
-        if v.ndim == 2:
-            mean = mean.reshape(1, -1)
-            std = std.reshape(1, -1)
-        return mean + v * std
-
-    def copy_stats(self, other):
-        self.set_mean(other.mean)
-        self.set_std(other.std)
