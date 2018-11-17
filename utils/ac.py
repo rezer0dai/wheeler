Encoder interface normalization; mcar + pendelume poc; not working yet, need some fix todo..

utils/ac.py
@@ -51,5 +51,5 @@ class ActorCritic(nn.Module):
         states, _ = self.norm(states, history_context)
         return self.critic[ind](states, actions)
 
-    def features(self, states):
+    def extract_features(self, states):
         return self.norm.extract_features(states)

utils/bot.py
@@ -19,10 +19,10 @@ class Bot(SoftUpdateNetwork):
         self.ac_explorer = ActorCritic(
                 encoder,
                 [Actor(
-                    encoder.out_size(), action_size, wrap_action, cfg
+                    encoder.total_size(), action_size, wrap_action, cfg
                     ) for i in range(1 if not cfg['detached_actor'] else cfg['n_simulations'])],
                 [Critic(
-                    encoder.out_size(), action_size, wrap_value, cfg
+                    encoder.total_size(), action_size, wrap_value, cfg
                     ) for i in range(cfg['n_simulations'])])
 
         self.attention = None if not self.cfg['attention_enabled'] else SimulationAttention(state_size, action_size, cfg)
@@ -38,10 +38,10 @@ class Bot(SoftUpdateNetwork):
         self.ac_target = ActorCritic(
                 encoder,
                 [Actor(
-                    encoder.out_size(), action_size, wrap_action, cfg
+                    encoder.total_size(), action_size, wrap_action, cfg
                     )],
                 [Critic(
-                    encoder.out_size(), action_size, wrap_value, cfg
+                    encoder.total_size(), action_size, wrap_value, cfg
                     ) for i in range(cfg['n_simulations'])])
         # sync
         self.sync_explorers(update_critics=True)
@@ -107,8 +107,7 @@ class Bot(SoftUpdateNetwork):
         self.sync_explorers() # load only alpha actor, keep critics our own
 
     def reevaluate(self, ind, states, actions):
-        s, f = self.ac_explorer.actor[ind].extract_features(states)
-        p = self.ac_explorer.actor[ind](
-                s,
-                torch.tensor(f)).log_prob(torch.tensor(actions)).detach().cpu().numpy()
+        s, f = self.ac_explorer.extract_features(states)
+        p = self.ac_explorer.actor[ind](s).log_prob(
+                torch.tensor(actions)).detach().cpu().numpy()
         return f, p

utils/encoders.py
@@ -1,46 +1,73 @@
 import abc
 import numpy as np
 
+import torch
+
 from utils.rbf import *
 from utils.normalizer import *
 
 class IEncoder(nn.Module):
-    def __init__(self):
+    def __init__(self, cfg):
         super().__init__()
+        self.n_history = cfg['history_count']
+        self.n_features = cfg['history_features']
 
     @abc.abstractmethod
     def out_size(self):
         pass
+
+    def count(self):
+        return self.n_history
+
+    def total_size(self):
+        return self.out_size() * self.count()
 
     @abc.abstractmethod
     def forward(self, states, history):
         pass
 
+    def has_features(self):
+        return False
+
     def extract_features(self, states):
-        feats = np.zeros(len(states), 1, 1, self.cfg['history_features'])
+        feats = torch.zeros(len(states), 1, 1, self.n_features)
         return self.forward(states, feats)
 
+# better to rethink design of this ~ beacuse of RNN ~ features, multiple ? dont over-engineer though...
 class StackedEncoder(IEncoder):
-    def __init__(self, size_in, encoder_a, encoder_b):
-        super().__init__()
+    def __init__(self, cfg, size_in, encoder_a, encoder_b):
+        super().__init__({ 'history_count' : 1, 'history_features' : cfg['history_features'] }) # well this is questionable .. rethink .. redo
         self.size = size_in
         self.encoder_a = encoder_a
         self.encoder_b = encoder_b
+        assert not self.encoder_a.has_features() or not self.encoder_a.has_features(), "only one RNN is allowed in encoder!"
+        assert not self.encoder_a.has_features(), "Currently RNN can be only *last* layer of encoder!!"
+
     def out_size(self):
         return self.encoder_b.out_size()
+
+    def has_features(self):
+        return self.encoder_a.has_features() or self.encoder_a.has_features()
+
     def forward(self, states, history):
         size = states.size(0)
         states, history = self.encoder_a(states.reshape(-1, self.size), history)
         states = states.reshape(size, -1)
         return self.encoder_b(states, history)
 
+    def extract_features(self, states):
+        states, features = self.encoder_a.extract_features(states)
+        if self.encoder_b.has_features(): # states already encoded, now extract feats!
+            return self.encoder_b.extract_features(states)
+        return states, features # no RNN present defaults feats are OK ..
+
 class RBFEncoder(IEncoder):
-    def __init__(self, env, gamas, components, sampler = None):
-        super().__init__()
+    def __init__(self, cfg, env, gamas, components, sampler = None):
+        super().__init__(cfg)
         self.size = len(env.reset())
         self.encoder = RbfState(env, gamas, components, sampler)
     def out_size(self):
-        return self.encoder.out_size()
+        return self.encoder.size
     def forward(self, states, history):
         size = states.size(0)
         states = self.encoder.transform(states.reshape(-1, self.size))
@@ -48,7 +75,7 @@ class RBFEncoder(IEncoder):
 
 class BatchNormalizer2D(IEncoder):
     def __init__(self, cfg, state_size):
-        super().__init__()
+        super().__init__(cfg)
         self.osize = state_size
         self.size = cfg['her_state_size'] + state_size * cfg['history_count']
         self.bn = nn.BatchNorm1d(self.size)
@@ -62,8 +89,9 @@ class BatchNormalizer2D(IEncoder):
         self.train()
         return out, history
 class BatchNormalizer3D(IEncoder):
-    def __init__(self, state_size):
-        super().__init__()
+    def __init__(self, cfg, state_size):
+        assert 0 == cfg['her_state_size'], "batchnorm on 3D input while HER active is incompatible!"
+        super().__init__(cfg)
         self.bn = nn.BatchNorm1d(state_size)
         self.size = state_size
     def out_size(self):
@@ -71,11 +99,16 @@ class BatchNormalizer3D(IEncoder):
     def forward(self, states, history):
         full_shape = states.shape
         states = states.reshape(states.size(0), self.size, -1)
-        return self.bn(states).reshape(full_shape), history
+        if states.size(0) > 1:
+            return self.bn(states).reshape(full_shape), history
+        self.eval() # this must be not called for training trolol
+        out = self.bn(states).reshape(full_shape)
+        self.train()
+        return out, history
 
 class GlobalNormalizerWGrads(IEncoder):
-    def __init__(self, state_size):
-        super().__init__()
+    def __init__(self, cfg, state_size):
+        super().__init__(cfg)
         self.bn = Normalizer(state_size)
     def out_size(self):
         return self.bn.size
@@ -85,8 +118,8 @@ class GlobalNormalizerWGrads(IEncoder):
         self.bn.update(states)
         return self.bn.normalize(states).reshape(full_shape), history
 class GlobalNormalizer(IEncoder):
-    def __init__(self, state_size):
-        super().__init__()
+    def __init__(self, cfg, state_size):
+        super().__init__(cfg)
         self.bn = Normalizer(state_size)
     def out_size(self):
         return self.bn.size

utils/rnn.py
@@ -1,22 +1,39 @@
 from utils.encoders import IEncoder
 
+import numpy as np
+
 import torch
 import torch.nn as nn
 
+# rethink multiple layered RNN; but dont increase much of complexity
+# should be done easily, see GRU vs LSTM cheat on n_features ( // 2 for LSTM )
+# so coupling hidden states after forward, and decouping it before passing to rnn
+
 class GRUEncoder(IEncoder):
-    def __init__(self, state_size, her_state, history_count, n_features):
-        super().__init__()
-        self.state_size = state_size
-        self.her_state = her_state
-        self.history_count = history_count
-        self.n_features = n_features
+    def __init__(self, cfg, state_size):
+        """
+        state_size must be total size : state_size * history_count
+         - therefore in case of encoder before, then encoder.total_size() is OK
+         - in case we feed directly outputs from environment then len(env.reset()) * history_count
+
+         - remember now RNN must be last layer, aka no normalization / encoding features afterwards!
+           - unless in actor/critic network itself
+        """
+        super().__init__({ 'history_count' : 1, 'history_features' : cfg['history_features'] })
+        self.her_state = cfg['her_state_size']
+        self.n_features = cfg['history_features']
+        self.history_count = cfg['history_count']
+        self.state_size = state_size // self.history_count
 
         self.rnn = nn.GRU(
-                state_size, n_features,
+                self.state_size, self.n_features,
                 batch_first=True,
                 bidirectional=False,
 #                dropout=.2,
                 num_layers=1)
+
+    def has_features(self):
+        return True
 
     def out_size(self):
         return self.n_features
@@ -32,12 +49,68 @@ class GRUEncoder(IEncoder):
         return hidden.view(states.size(0), -1), features
 
     def extract_features(self, states):
-        s = states[:, # we want to get context from before our state
-                self.her_state:self.her_state_size+self.state_size:]
+        states = states[:, # we want to get context from before our state
+                self.her_state:self.her_state+self.state_size:]
 
         hidden = torch.zeros(1, 1, self.n_features)
 
-        out, _ = self.rnn(s.unsqueeze(0), hidden)
+        out, _ = self.rnn(states.unsqueeze(0), hidden)
         features = [h.reshape(1, 1, -1).detach().cpu().numpy() for h in out.squeeze(0)]
         ret = [ hidden.numpy() ] + features[:-1]
-        return states, ret
+        return torch.tensor(features).view(states.size(0), -1), ret
+
+class LSTMEncoder(IEncoder):
+    def __init__(self, cfg, state_size):
+        """
+        state_size must be total size : state_size * history_count
+         - therefore in case of encoder before, then encoder.total_size() is OK
+         - in case we feed directly outputs from environment then len(env.reset()) * history_count
+
+         - remember now RNN must be last layer, aka no normalization / encoding features afterwards!
+           - unless in actor/critic network itself
+        """
+        super().__init__({ 'history_count' : 1, 'history_features' : cfg['history_features'] })
+        self.her_state = cfg['her_state_size']
+        self.n_features = cfg['history_features'] // 2
+        self.history_count = cfg['history_count']
+        self.state_size = state_size // self.history_count
+
+        self.rnn = nn.LSTMCell(
+                self.state_size, self.n_features)
+
+    def has_features(self):
+        return True
+
+    def out_size(self):
+        return self.n_features
+
+    def forward(self, states, hidden):
+        x = states[:, self.her_state:]
+        x = x.view(x.size(0), self.history_count, -1)
+
+        out, feature = self.rnn(x.transpose(0, 1)[0, :], hidden.view(2, states.size(0), -1))
+        features = torch.cat([out, feature], 1).view(1, 1, -1)
+        for s in x.transpose(0, 1)[1:, :]:
+            out, feature = self.rnn(s, (out, feature))
+
+        return out.view(states.size(0), -1), features
+
+    def extract_features(self, states):
+        states = states[:, # we want to get context from before our state
+                self.her_state:self.her_state+self.state_size:]
+
+        hidden = torch.zeros(2, 1, self.n_features)
+        feature = hidden.view(1, 1, -1)
+        features = [feature.detach().cpu().numpy()]
+
+        outs = []
+        for x in states:
+            x = x.view(1, -1)
+            out, feature = self.rnn(x, hidden)
+            hidden = (out, feature)
+            feature = torch.cat(hidden, 1).view(1, 1, -1)
+
+            outs.append(out.reshape(-1).detach().cpu().numpy())
+            features.append(feature.detach().cpu().numpy())
+
+        return torch.tensor(outs).view(len(states), -1), features[:-1]

utils/task.py
@@ -8,9 +8,6 @@ from utils.replay import ReplayBuffer
 from collections import deque
 
 class Task(object, metaclass=abc.ABCMeta):
-    ep_count = 0
-    training_finished = False
-
     def __init__(self, cfg, env, objective_id, bot_id, action_low, action_high):
         self.cfg = cfg
         self.env = env
@@ -21,15 +18,7 @@ class Task(object, metaclass=abc.ABCMeta):
         self.action_high = action_high
 
         self.env.register(self.bot_id, self.objective_id)
-# globals
-    def iter_count(self):
-        return Task.ep_count
-    def episode_counter(self):
-        return Task.ep_count
-    def learned(self):
-        return Task.training_finished
-    def training_status(self, status):
-        Task.training_finished = status
+        self.ep_count = 0
 # local info
     def name(self):
         return self.cfg['task']+"-def[%i]"%self.objective_id
@@ -41,12 +30,14 @@ class Task(object, metaclass=abc.ABCMeta):
         if 1 == torch.cuda.device_count():
             return "cuda"
         return "cuda:%i"%(1 + (self.objective_id % (torch.cuda.device_count() - 1)))
+    def iter_count(self):
+        return self.ep_count
 
     def step(self, action):
         r = 0
         for i, act in enumerate(action):
             for _ in range(self.cfg['action_repeat']):
-                Task.ep_count += int(1 == self.objective_id)
+                self.ep_count += int(1 == self.objective_id)
                 act = np.clip(act, self.action_low, self.action_high)
                 a, state, reward, done, good = self.step_ex(act)
                 r += reward
@@ -54,7 +45,6 @@ class Task(object, metaclass=abc.ABCMeta):
                     break
             if any(a != act): # user want to change this action ( wrt gradient for training )
                 action[i] = a
-            #self.prev_state = self.env.pose
             if done:
                 break
         return np.array(action), state, r, done, good
@@ -105,6 +95,12 @@ class Task(object, metaclass=abc.ABCMeta):
         action[a] = 1.
         return action, a
 
+    def seed(self):
+        return self._seed
+
+    def update_goal(self, rewards, states, n_states, updates):
+        return rewards, states, n_states
+
     def her_state(self, ind = 0):
         return []
 
@@ -118,14 +114,8 @@ class Task(object, metaclass=abc.ABCMeta):
         self._seed = seed
         return [self.env_reset(seed)]
 
-    def seed(self):
-        return self._seed
-
     def env_reset(self, seed):
         return self.env.reset(self.bot_id, self.objective_id, seed)
-
-    def update_goal(self, rewards, states, n_states, updates):
-        return rewards, states, n_states
 
     @abc.abstractmethod
     def goal_met(self, state, n_steps):